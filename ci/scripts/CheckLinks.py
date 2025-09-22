0bed5507b4cbc1eab0a21b510b085df68eb5d15c
MeteIsCoding <mete.yalcin.05@gmail.com>
2025-09-22 16:36:43 +0200

    Add recursive markdown file locater method

---
+++ ci/scripts/CheckLinks.py
@@ -0,0 +1,27 @@
+# ================================ #
+# ==== Recherche des fichiers ==== #
+# ================================ #
+
+import os
+
+
+def FindAllMarkdown(dossier):
+    """
+    Parcourt le dossier donné de manière recursive pour trouver tous les fichiers markdown
+
+    Args :
+        dossier/ :  le chemin du dossier à parcourir
+
+    Returns :
+        list : la liste des chemins complets des fichiers Markdown trouvés
+    """
+    markdowns = []
+
+    # On parcourt tous les directory et fichiers enfants
+    for racine, fichiers in os.walk(dossier):
+        for fichier in fichiers:
+
+            # Si le fichier se termine par un .md alors on l'ajoute à la liste
+            if fichier.endswith('.md'):
+                markdowns.append(os.path.join(racine, fichier))
+    return markdowns
