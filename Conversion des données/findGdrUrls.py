c5e2e7d851d4e708b44ea0967c432d7c28fcb098
Matthias LIONARDO <137904345+mtthIA@users.noreply.github.com>
2025-09-24 14:45:27 +0000

    test url

--- Conversion des données/findGdrUrls.py
+++ Conversion des données/findGdrUrls.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def main():
     # Obtenir tous les fichiers .md dans le dossier content et ses sous-dossiers
-    content_path = "content"
+    content_path = "../content"
     if not os.path.exists(content_path):
         print(f"Erreur: Le dossier '{content_path}' n'existe pas")
         return
